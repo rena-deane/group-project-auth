33ee053705fd1a317766490b2c3c731e759858f8
Marcus Crane <marcus@thingsima.de>
2016-07-22 11:09:38 +1200

    Added .gitkeep files plus basic express setup

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 var express = require('express')
+var path = require('path')
+var bodyParser = require('body-parser')
+var passport = require('passport')
+var session = require('express-session')
+
+var routes = require('./routes/index')
+var setupPassport = require('./passportSetup')
+var app = express()
+
+app.set('views' path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(session({ secret: 'supersecretpassword123' }))
+app.use(passport.initialize())
+app.use(passport.session())
+setupPassport()
+
+app.use('/', routes)
+
+module.exports = app
