df2c1fcd50618495242746bb59c4a0d142104960
Marcus Crane <marcus@thingsima.de>
2016-07-22 14:10:56 +1200

    Basic auth up and running

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express')
+var passport = require('passport')
 var router = express.Router()
 
 router.get('/', function(req, res, next) {
@@ -9,25 +10,30 @@ router.get('/login', function(req, res, next) {
   res.render('login')
 })
 
-router.get('/register', function(req, res, next) {
-  res.render('register')
-})
-
-router.get('/user/:username', function(req, res, next) {
+router.get('/user', function(req, res, next) {
+  console.log('hi')
   res.render('profile')
-}
+})
 
-router.post('/user', function(req, res, next) {
+router.post('/user', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/logout'
+}))
+
+router.get('/user/new', function(req, res, next) {
   console.log(req)
+  res.render('register')
   res.sendStatus(200)
 })
 
-router.post('/user/new', function(req, res, next) {
-  console.log(req)
+router.get('/logout', function(req, res, next) {
+  console.log('logged out')
   res.sendStatus(200)
 })
 
-router.post('/logout', function(req, res, next) {
-  console.log(req)
-  res.sendStatus(200)
-})
+// router.post('/logout', function(req, res, next) {
+//   console.log('logged out')
+//   res.sendStatus(200)
+// })
+
+module.exports = router
